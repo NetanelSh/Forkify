a40fa64ca7d621ead7e0dd400aae8ce38f8cc7f4
Netanel Sheinbin <sheinbinn@gmail.com>
2018-12-01 21:11:59 +0200

    Add MVC work

---
+++ src/js/models/Search.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const key = "ec4616f31cc5cdd4d7cbc80ba4fdb1e8";
+        try {
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.results = res.data.recipes;
+            console.log(this.results);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
