a0b7b534a4607c0cc08f39208e3bdf198ac219c9
Netanel Sheinbin <sheinbinn@gmail.com>
2018-12-02 16:24:52 +0200

    Search Model

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,47 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+// Global State of the app
+// Search
+// Recipe
+// Shopping List
+// Liked
+
+const state = {};
+
+const controlSearch = async () => {
+    // Get query from view
+    const query = searchView.getInput();
+
+    if (query) {
+        // New search object and add to state
+        state.search = new Search(query);
+
+        // Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+        // search for recipes
+        await state.search.getResults();
+    
+        // render UI with results
+        clearLoader();
+        searchView.renderResults(state.search.results);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if ( btn ) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.results, goToPage);
+    }
+})
